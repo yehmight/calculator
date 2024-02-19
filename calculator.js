f89930efef697a623d8ccae23a7fc0c042bd77bb
yehmight <145411012+yehmight@users.noreply.github.com>
2024-02-19 19:54:34 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+
+
+// function seven(){
+//     document.forms.calc.display.value += '7';
+// }
+
+function Numbers(num){
+    document.forms.calc.display.value +=num;
+}
+
+
+// function clear(){
+//     document.forms.calc.display.value ='';
+// }
